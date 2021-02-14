90c3d5f391b3ace86fe636d6544d1779160f10f1
Nina Jade <jaderiver64@gmail.com>
2021-02-14 17:42:49 -0600

    failing test for manager behave as expected

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,2 +1,28 @@
 const Employee = require('../lib/Employee.js');
 const Manager = require('../lib/Manager.js');
+
+
+test('create a manager object ', () => {
+
+    const manager = new Manager('Shirelle Monae', 'shirelle.m@fauxmail.com');
+
+    expect(manager.name).toStrictEqual('smeg');// Shirelle Monae
+    expect(manager.email).toStrictEqual('smeg');// shirelle.m@fauxmail.com
+
+    expect(employee.id).toStrictEqual(expect.any(String));// Number
+
+});
+
+test("get manager's role ", ()=>{
+ const manager = new Manager('Shirelle Monae', 'shirelle.m@fauxmail.com');
+
+expect(manager.getRole).toStrictEqual('Employee');// Manager
+
+});
+
+test("get manager's office number ",()=>{
+     const manager = new Manager('Shirelle Monae', 'shirelle.m@fauxmail.com');
+
+expect(manager.getOfficeNumber).toStrictEqual('smeg');// (expect.any(Number)
+
+})
