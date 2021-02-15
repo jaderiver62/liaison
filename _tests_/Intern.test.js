a6912a2639d942bd6bb906bc8b1b0ed7f66fd2f8
Nina Jade <jaderiver64@gmail.com>
2021-02-14 21:29:56 -0600

    Intern class built and testing

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,27 @@
+const Employee = require('../lib/Employee.js');
+const Intern = require('../lib/Intern.js');
+
+
+test('create an intern object ', () => {
+
+    const intern = new Intern('Shirelle Monae', 'shirelle.m@fauxmail.com', 'R34', 'City College');
+
+    expect(intern.name).toStrictEqual('Shirelle Monae');
+    expect(intern.email).toStrictEqual('shirelle.m@fauxmail.com');
+
+    expect(intern.id).toStrictEqual(expect.any(String));
+
+});
+
+test("get intern's role ", () => {
+    const intern = new Intern('Shirelle Monae', 'shirelle.m@fauxmail.com', 'R34', 'City College');
+
+
+    expect(intern.getRole()).toEqual(expect.stringContaining('City College'));
+
+});
+
+test("get intern's school ", () => {
+    const intern = new Intern('Shirelle Monae', 'shirelle.m@fauxmail.com', 'R34', 'City College');
+    expect(intern.getGitHub()).toEqual(expect.stringContaining('City College'));
+});
